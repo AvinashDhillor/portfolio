0e04b79a10e920127dd9df761d68121629c2b18e
Avinash Dhillor <avinashdhillor24@gmail.com>
2018-12-03 07:30:21 +0530

    Reading from json file finished using fetch

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-// // console.log(window.location.pathname);
-// const PATH = window.location.pathname;
-// fetch(`./projects/sample.json`)
-//   .then(res => {
-//     return res;
-//   })
-//   .then(data => {
-//     let obj = JSON.parse(data);
-//     console.log(obj.topic);
-//   })
-//   .catch(e => {});
+fetch('./projects/sample1.json')
+  .then(res => {
+    return res.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(e => {
+    console.log(e);
+  });
