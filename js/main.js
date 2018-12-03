100f305cfbd5807f2c3708226c50487fad16e64b
Avinash Dhillor <avinashdhillor24@gmail.com>
2018-12-03 06:06:37 +0530

    header fixed

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// // console.log(window.location.pathname);
+// const PATH = window.location.pathname;
+// fetch(`./projects/sample.json`)
+//   .then(res => {
+//     return res;
+//   })
+//   .then(data => {
+//     let obj = JSON.parse(data);
+//     console.log(obj.topic);
+//   })
+//   .catch(e => {});
